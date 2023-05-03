604f151cb3a2ef1abcbf995a3b449fd633957f41
Sam <sam.leavin@gmail.com>
2023-05-03 09:21:35 -0400

    add generic bash script

---
+++ assets/generic-bash-script.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Set bash unofficial strict more
+# For more info, see: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "foo"
