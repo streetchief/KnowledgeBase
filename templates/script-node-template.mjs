58efbfe2d85eb86a507c803937bd4415d5a080ba
Sam Leavin <sam.leavin@gmail.com>
2025-12-31 12:13:34 -0500

    update scripts

--- templates/script-node-template.mjs
+++ templates/script-node-template.mjs
@@ -5,12 +5,13 @@
 // manipulating files: https://nodejs.dev/en/learn/nodejs-file-stats/
 // n.b. The entire contents of JavaScript modules are automatically in strict mode, with no statement needed to initiate it.
 
-import { opendir } from 'fs/promises';
+import { opendir } from 'node:fs/promises';
 
 try {
   const dir = await opendir('./');
-  for await (const dirent of dir)
+  for await (const dirent of dir) {
     console.log(dirent.name);
+  }
 } catch (err) {
   console.error(err);
 }
